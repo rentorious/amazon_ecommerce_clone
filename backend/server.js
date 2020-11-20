6614160d664a8633432097950639da6d5a6e62f3
Ognjen Popovic <rentorious@gmail.com>
2020-11-20 15:36:15 +0100

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,14 @@ app.get("/api/config/paypal", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
+// app.get("/", (req, res) => {
+//   res.send("Server is ready");
+// });
 
 const port = process.env.PORT || 5000;
 
